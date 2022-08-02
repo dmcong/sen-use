e672683aa160d9d4235a75e7825b2072004881cd
The Vinh <vinhnguyen@descartes.network>
2022-08-02 14:46:55 +0700

    rm test component

--- packages/render/src/view/index.tsx
+++ packages/render/src/view/index.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useWallet, createPDB } from '@sentre/senhub'
-import { MintSelection } from '@sen-use/components'
+import { MintSelection, NFTSelection, searchNFTType } from '@sen-use/components'
 
 import { Row, Col, Typography, Button, Space } from 'antd'
 import IonIcon from '@sentre/antd-ionicon'
@@ -9,8 +9,6 @@ import IonIcon from '@sentre/antd-ionicon'
 import { AppDispatch, AppState } from 'model'
 import { increaseCounter } from 'model/main.controller'
 import configs from 'configs'
-import { NFTSelection } from './NFT'
-import { searchNFTType } from './NFT/NFTSelection'
 
 const {
   manifest: { appId },
@@ -28,9 +26,6 @@ const View = () => {
   useEffect(() => {
     if (pdb) pdb.setItem('counter', counter)
   }, [pdb, counter])
-  const onSelectNFT = (mintAddress: string) => {
-    console.log('Address: ', mintAddress)
-  }
 
   return (
     <Row gutter={[24, 24]} align="middle">
@@ -50,14 +45,10 @@ const View = () => {
         <Button onClick={increase}>Increase</Button>
       </Col>
       <MintSelection />
-      <NFTSelection
-        collectionAddress="3NKWipp36DnCyQk2LQmXbret2U2M816GWBCvHQAaMyFT"
-        onSelect={onSelectNFT}
-      />
+      <NFTSelection />
       <NFTSelection
         searchNFTby={searchNFTType.collections}
         title="Select NFT collection"
-        onSelect={onSelectNFT}
       />
     </Row>
   )
