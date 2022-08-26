f0969500789349d740dc6e6b584b94c981e0937f
minhcong <83624415+dmcong@users.noreply.github.com>
2022-08-26 19:53:22 +0700

    update get mint price

--- packages/app/src/hooks/mint/useGetPrice.ts
+++ packages/app/src/hooks/mint/useGetPrice.ts
@@ -1,58 +1,14 @@
 import { useCallback } from 'react'
-import { tokenProvider, util } from '@sentre/senhub'
+import { tokenProvider } from '@sentre/senhub'
 import { Address, BN } from '@project-serum/anchor'
-import { utilsBN, DataLoader } from '@sen-use/web3'
+import { utilsBN } from '@sen-use/web3'
 
 import { useGetMintDecimals } from './useGetMintDecimals'
 
-const USDC_ADDRESS = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
-
-const getJupiterPrice = async (mintAddress: string, decimals: number) => {
-  try {
-    const priceUrl = `https://quote-api.jup.ag/v1/quote?inputMint=${USDC_ADDRESS}&outputMint=${mintAddress}&amount=1000000&slippage=1`
-    const { data } = await DataLoader.load(
-      'getJupiterPrice' + mintAddress,
-      async () => (await fetch(priceUrl)).json(),
-    )
-    const outAmount = new BN(data[0].outAmount)
-    const outAmountUI = await utilsBN.undecimalize(outAmount, decimals)
-    return 1 / Number(outAmountUI)
-  } catch (error) {
-    return 0
-  }
-}
-
-const getCgkPrice = async (mintAddress: string) => {
-  try {
-    const price = await DataLoader.load(
-      'getCgkPrice' + mintAddress,
-      async () => {
-        const token = await tokenProvider.findByAddress(mintAddress)
-        const ticket = token?.extensions?.coingeckoId
-        const cgkData = await util.fetchCGK(ticket)
-        return cgkData.price as number
-      },
-    )
-    return price
-  } catch (error) {
-    return 0
-  }
-}
-
 export const useGetMintPrice = () => {
-  const getMintDecimals = useGetMintDecimals()
-
-  const getPrice = useCallback(
-    async (mintAddress: Address) => {
-      const cgkPrice = await getCgkPrice(mintAddress.toString())
-      if (!!Number(cgkPrice)) return cgkPrice
-
-      const decimals = await getMintDecimals({ mintAddress })
-      return getJupiterPrice(mintAddress.toString(), decimals)
-    },
-    [getMintDecimals],
-  )
-
+  const getPrice = useCallback(async (mintAddress: Address) => {
+    return tokenProvider.getPrice(mintAddress)
+  }, [])
   return getPrice
 }
 
